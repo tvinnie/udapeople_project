6179c0db08456d24bb8d110b9f2d292369839937
tvinnie <tvinnie77@gmail.com>
2023-02-06 13:31:01 +0300

    infrastructure migration

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST-database-1.ctupcjqkfsj5.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=tvinnie-dbpost.cpu90y2ye7ko.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD-postgres
-export TYPEORM_DATABASE-postgres
+export TYPEORM_PASSWORD=seminary
+export TYPEORM_DATABASE=tvinniedb
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
-export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_MIGRATIONS_DIR=./src/migrations  
